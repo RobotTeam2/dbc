f7589e9804a4cf1005d1ce0be8095b63684d1dce
Wator Vapor <watorvapor@gmail.com>
2017-10-04 21:01:19 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -3,7 +3,7 @@
     <div class="col-sm ">
       <form method="POST" action="/uart/forword">
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
       </form>
     </div>
   </div>
@@ -11,19 +11,19 @@
     <div class="col">
       <form method="POST" action="/uart/left">
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
       </form>
     </div>
     <div class="col">
       <form method="POST" action="/uart/stop">
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-lg btn-block btn-danger"><i class="material-icons">stop</i></button>
+        <button type="submit" class="btn btn-lg btn-danger"><i class="material-icons">stop</i></button>
       </form>
     </div>
     <div class="col">
       <form method="POST" action="/uart/right">
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
       </form>
     </div>
   </div>
@@ -31,7 +31,7 @@
     <div class="col-sm">
       <form method="POST" action="/uart/back">
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_down</i></button>
+        <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_down</i></button>
       </form>
     </div>
   </div>
