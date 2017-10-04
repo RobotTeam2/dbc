3c15db9cef1707057ad4778af5c83b83e5bbddab
Wator Vapor <watorvapor@gmail.com>
2017-10-04 20:51:22 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
   <div class="row">
     <div class="col-sm">
-      <form  class="mt-2 mb-2" method="POST" action="/uart/up">
+      <form method="POST" action="/uart/forword">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
       </form>
@@ -11,19 +11,31 @@
   <div class="row" style="height:2%;"></div>
   <div class="row">
     <div class="col-sm">
-      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
+      <form method="POST" action="/uart/left">
+        {{ csrf_field() }}
+        <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
+      </form>
     </div>
     <div class="col">
-      <button type="button" class="btn btn-lg btn-block btn-danger"><i class="material-icons">stop</i></button>
+      <form method="POST" action="/uart/stop">
+        {{ csrf_field() }}
+        <button type="button" class="btn btn-lg btn-block btn-danger"><i class="material-icons">stop</i></button>
+      </form>
     </div>
     <div class="col">
-      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
+      <form method="POST" action="/uart/right">
+        {{ csrf_field() }}
+        <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
+      </form>
     </div>
   </div>
   <div class="row" style="height:2%;"></div>
   <div class="row">
     <div class="col-sm">
-      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_down</i></button>
+      <form method="POST" action="/uart/back">
+        {{ csrf_field() }}
+        <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_down</i></button>
+      </form>
     </div>
   </div>
 </div>
