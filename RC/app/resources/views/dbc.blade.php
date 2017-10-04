d620b7acd92f7d0dc63286b63483ef86f76ce2f3
Wator Vapor <watorvapor@gmail.com>
2017-10-04 20:29:36 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -1,8 +1,7 @@
-
 <div class="container" style="height:10%;"></div>
 <div class="container">
   <div class="row">
-    <div class="col-lg-1 offset-lg-5">
+    <div class="col-lg">
       <button type="button" class="btn btn-lg btn-block btn-primary" onclick="forward()"><i class="material-icons">fast_forward</i></button>
     </div>
   </div>
@@ -11,25 +10,25 @@
 <hr/>
 <div class="container">
   <div class="row">
-    <div class="col-sm-1 offset-sm-5">
+    <div class="col-sm">
       <button type="button" class="btn btn-lg btn-block btn-success" onclick="up()"><i class="material-icons">keyboard_arrow_up</i></button>
     </div>
   </div>
   <div class="row" style="height:2%;"></div>
   <div class="row">
-    <div class="col-sm-1 offset-sm-4">
+    <div class="col-sm">
       <button type="button" class="btn btn-lg btn-block btn-success" onclick="left()"><i class="material-icons">keyboard_arrow_left</i></button>
     </div>
-    <div class="col-sm-1">
+    <div class="col">
       <button type="button" class="btn btn-lg btn-block btn-danger" onclick="stop()"><i class="material-icons">stop</i></button>
     </div>
-    <div class="col-sm-1">
+    <div class="col">
       <button type="button" class="btn btn-lg btn-block btn-success" onclick="right()"><i class="material-icons">keyboard_arrow_right</i></button>
     </div>
   </div>
   <div class="row" style="height:2%;"></div>
   <div class="row">
-    <div class="col-sm-1 offset-sm-5">
+    <div class="col-sm">
       <button type="button" class="btn btn-lg btn-block btn-success" onclick="down()"><i class="material-icons">keyboard_arrow_down</i></button>
     </div>
   </div>
