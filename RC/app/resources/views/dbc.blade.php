1de50a7b70848911844812d3ede9494dfc2f4030
Wator Vapor <watorvapor@gmail.com>
2017-10-04 21:00:12 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -1,16 +1,14 @@
-<div class="container" style="height:10%;"></div>
 <div class="container">
-  <div class="row">
-    <div class="col-sm">
+  <div class="row align-items-start">
+    <div class="col-sm ">
       <form method="POST" action="/uart/forword">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
       </form>
     </div>
   </div>
-  <div class="row" style="height:2%;"></div>
-  <div class="row">
-    <div class="col-sm">
+  <div class="row align-items-center">
+    <div class="col">
       <form method="POST" action="/uart/left">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
@@ -29,8 +27,7 @@
       </form>
     </div>
   </div>
-  <div class="row" style="height:2%;"></div>
-  <div class="row">
+  <div class="row align-items-end">
     <div class="col-sm">
       <form method="POST" action="/uart/back">
         {{ csrf_field() }}
