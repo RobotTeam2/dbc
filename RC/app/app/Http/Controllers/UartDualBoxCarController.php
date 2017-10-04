938a1bba37c57cdb9fd2c49d26c12f31440b365b
Wator Vapor <watorvapor@gmail.com>
2017-10-04 23:24:04 +0900

    Update UartDualBoxCarController.php

--- RC/app/app/Http/Controllers/UartDualBoxCarController.php
+++ RC/app/app/Http/Controllers/UartDualBoxCarController.php
@@ -8,23 +8,29 @@ use Illuminate\Support\Facades\Redis;
 class UartDualBoxCarController extends Controller
 {
     //
+    const CHANNEL = '/dbc/webui2uart';
     public function forword(Request $request) {
+        Redis::publish(CHANNEL, 'forword');
         return redirect()->back();
     }
     //
     public function back(Request $request) {
+        Redis::publish(CHANNEL, 'back');
         return redirect()->back();
     }
     //
     public function left(Request $request) {
+        Redis::publish(CHANNEL, 'left');
         return redirect()->back();
     }
     //
     public function right(Request $request) {
+        Redis::publish(CHANNEL, 'right');
         return redirect()->back();
     }
     //
     public function stop(Request $request) {
+        Redis::publish(CHANNEL, 'stop');
         return redirect()->back();
     }
 }
