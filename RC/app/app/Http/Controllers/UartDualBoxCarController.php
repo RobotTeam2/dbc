1f0e6ae1d656011a30f4729a2bac5c2ee259fa16
Wator Vapor <watorvapor@gmail.com>
2017-10-04 20:55:08 +0900

    Update UartDualBoxCarController.php

--- RC/app/app/Http/Controllers/UartDualBoxCarController.php
+++ RC/app/app/Http/Controllers/UartDualBoxCarController.php
@@ -7,4 +7,23 @@ use Illuminate\Http\Request;
 class UartDualBoxCarController extends Controller
 {
     //
+    public function forword(Request $request) {
+        return redirect()->back();
+    }
+    //
+    public function back(Request $request) {
+        return redirect()->back();
+    }
+    //
+    public function left(Request $request) {
+        return redirect()->back();
+    }
+    //
+    public function right(Request $request) {
+        return redirect()->back();
+    }
+    //
+    public function stop(Request $request) {
+        return redirect()->back();
+    }
 }
