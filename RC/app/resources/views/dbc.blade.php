de512c23efb7bff4ed5c8bbc292131eb21c8001a
Wator Vapor <watorvapor@gmail.com>
2017-10-04 23:13:06 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -1,7 +1,7 @@
 <div class="container mt-lg-5">
   <div class="row align-items-start justify-content-center">
     <div class="col-4 justify-content-center">
-      <form method="POST" action="/uart/forword">
+      <form method="POST" class="justify-content-center" action="/uart/forword">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
       </form>
