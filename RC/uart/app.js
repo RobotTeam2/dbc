efafff5efbf826719ccf4e66b950f2429ed6c8e0
Wator Vapor <watorvapor@gmail.com>
2017-10-04 23:33:21 +0900

    Update app.js

--- RC/uart/app.js
+++ RC/uart/app.js
@@ -1,27 +1,7 @@
-var bleno = require('bleno');
-var BlenoPrimaryService = bleno.PrimaryService;
-var EchoCharacteristic = require('./characteristic');
-console.log('bleno - dbc.rc.4wd');
-bleno.on('stateChange', function(state) {
-  console.log('on -> stateChange: ' + state);
-  if (state === 'poweredOn') {
-    bleno.startAdvertising('dbc.rc.4wd', ['ac5636ee-3d36-4afe-9662-ec47fbfe1dd0']);
-  } else {
-    bleno.stopAdvertising();
-  }
-});
-
-bleno.on('advertisingStart', function(error) {
-  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
-
-  if (!error) {
-    bleno.setServices([
-      new BlenoPrimaryService({
-        uuid: 'ac5636ee-3d36-4afe-9662-ec47fbfe1dd0',
-        characteristics: [
-          new EchoCharacteristic()
-        ]
-      })
-    ]);
-  }
+var redis      = require('redis');
+var subscriber = redis.createClient(6379, 'localhost');
+ 
+subscriber.subscribe('/dbc/webui2uart');
+subscriber.on("message", function(channel, message) {
+  console.log('message=<',message,'>');
 });
