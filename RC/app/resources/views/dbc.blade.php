9815a35b06a70c4a282714a67462e49930c1e0f8
Wator Vapor <watorvapor@gmail.com>
2017-10-04 21:07:57 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -8,19 +8,19 @@
     </div>
   </div>
   <div class="row align-items-center justify-content-center">
-    <div class="col">
+    <div class="col-4">
       <form method="POST" action="/uart/left">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
       </form>
     </div>
-    <div class="col">
+    <div class="col-4">
       <form method="POST" action="/uart/stop">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-danger"><i class="material-icons">stop</i></button>
       </form>
     </div>
-    <div class="col">
+    <div class="col-4">
       <form method="POST" action="/uart/right">
         {{ csrf_field() }}
         <button type="submit" class="btn btn-lg btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
