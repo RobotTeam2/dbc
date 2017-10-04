df926fc0a9130cd262fc03cf4eeaad8bf159d58c
Wator Vapor <watorvapor@gmail.com>
2017-10-04 23:40:23 +0900

    Update app.js

--- RC/uart/app.js
+++ RC/uart/app.js
@@ -1,7 +1,26 @@
 var redis      = require('redis');
-var subscriber = redis.createClient(6379, 'localhost');
- 
+var SerialPort = require("serialport").SerialPort
+
+
+
+var subscriber = redis.createClient(6379, 'localhost'); 
 subscriber.subscribe('/dbc/webui2uart');
 subscriber.on("message", function(channel, message) {
   console.log('message=<',message,'>');
+});
+
+var serialPort = new SerialPort("/dev/tty.usbmodem1411", {
+ baudrate: 112500
+});
+
+serialPort.on("open", function () {
+  console.log('open');
+ });
+
+serialPort.on('data', function(data) {
+  console.log('received data =<',data,'>');
+});
+
+serialPort.on('error', function(err) {
+  console.log('err =<',err,'>');
 });
