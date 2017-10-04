fae420195548bda9b22b4d38f627c6bf4a589de7
Wator Vapor <watorvapor@gmail.com>
2017-10-04 20:41:51 +0900

    Update dbc.blade.php

--- RC/app/resources/views/dbc.blade.php
+++ RC/app/resources/views/dbc.blade.php
@@ -2,25 +2,25 @@
 <div class="container">
   <div class="row">
     <div class="col-sm">
-      <button type="button" class="btn btn-lg btn-block btn-success" onclick="up()"><i class="material-icons">keyboard_arrow_up</i></button>
+      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_up</i></button>
     </div>
   </div>
   <div class="row" style="height:2%;"></div>
   <div class="row">
     <div class="col-sm">
-      <button type="button" class="btn btn-lg btn-block btn-success" onclick="left()"><i class="material-icons">keyboard_arrow_left</i></button>
+      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_left</i></button>
     </div>
     <div class="col">
-      <button type="button" class="btn btn-lg btn-block btn-danger" onclick="stop()"><i class="material-icons">stop</i></button>
+      <button type="button" class="btn btn-lg btn-block btn-danger"><i class="material-icons">stop</i></button>
     </div>
     <div class="col">
-      <button type="button" class="btn btn-lg btn-block btn-success" onclick="right()"><i class="material-icons">keyboard_arrow_right</i></button>
+      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_right</i></button>
     </div>
   </div>
   <div class="row" style="height:2%;"></div>
   <div class="row">
     <div class="col-sm">
-      <button type="button" class="btn btn-lg btn-block btn-success" onclick="down()"><i class="material-icons">keyboard_arrow_down</i></button>
+      <button type="button" class="btn btn-lg btn-block btn-success"><i class="material-icons">keyboard_arrow_down</i></button>
     </div>
   </div>
 </div>
