335f2fcac9f0bd98c153d09712b4c13aa6fc176d
Wator Vapor <watorvapor@gmail.com>
2017-10-05 11:45:49 +0900

    Update app.js

--- RC/uart/app.js
+++ RC/uart/app.js
@@ -4,12 +4,15 @@ var fs = require('fs');
 
 var files = fs.readdirSync('/dev/');
 console.log('files=<',files,'>');
+var uartDevice = false;
 files.forEach(function(file) {
   //console.log('file=<',file,'>');
   if(file.startsWith('ttyUSB')) {
     console.log('file=<',file,'>');
+    uartDevice = '/dev/' +file;
   }
 });
+console.log('uartDevice=<',uartDevice,'>');
 
 
 
@@ -19,7 +22,7 @@ subscriber.on("message", function(channel, message) {
   console.log('message=<',message,'>');
 });
 
-var serialPort = new SerialPort("/dev/ttyS0", {
+var serialPort = new SerialPort(uartDevice, {
  baudrate: 112500
 });
 
