8a64d5eb3e062dd526dd4ba40deb76c075efae03
Wator Vapor <watorvapor@gmail.com>
2017-10-06 18:35:13 +0900

    Update app.js

--- RC/ble/app.js
+++ RC/ble/app.js
@@ -2,10 +2,12 @@ var bleno = require('bleno');
 var BlenoPrimaryService = bleno.PrimaryService;
 var DBCCharacteristic = require('./characteristic');
 console.log('bleno - dbc.rc.4wd');
+var serviceID = '9a10ba1d-cd1c-4f00-9cca-1f3178d5fe8a';
+
 bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
   if (state === 'poweredOn') {
-    bleno.startAdvertising('dbc.rc.4wd', ['ac5636ee-3d36-4afe-9662-ec47fbfe1dd0']);
+    bleno.startAdvertising('dbc.rc.4wd', [serviceID]);
   } else {
     bleno.stopAdvertising();
   }
@@ -17,7 +19,7 @@ bleno.on('advertisingStart', function(error) {
   if (!error) {
     bleno.setServices([
       new BlenoPrimaryService({
-        uuid: 'ac5636ee-3d36-4afe-9662-ec47fbfe1dd0',
+        uuid: serviceID,
         characteristics: [
           new DBCCharacteristic()
         ]
